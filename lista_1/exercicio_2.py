1c4bd5f4f5275bb59f76e63f17a2c53f9c2cba8c
Jecielly <jeciellymota@outlook.com>
2022-11-02 11:32:56 -0300

    create: Calculo da area de um circulo

---
+++ lista_1/exercicio_2.py
@@ -0,0 +1,9 @@
+# O cliente vai me dar um raio de um círculo e quer o calculo da area 
+
+import math
+
+
+raio = float(input("Dê o raio do circulo que deseja calcular: "))
+area = math.pi * raio**2
+
+print(f"A area do circulo é de {area} ")
